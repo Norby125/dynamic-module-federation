fdc25451a8ce1f664027f3c4dff25e1f640207b3
Papp Norbert <pnorbert@itron.com>
2024-07-11 23:58:01 +0200

    .

--- remote/webpack.config.js
+++ remote/webpack.config.js
@@ -6,26 +6,26 @@ module.exports = withModuleFederationPlugin({
     './Component': '././src/app/app.component.ts',
   },
   shared: share({
-    '@angular-architects/module-federation': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/animations': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/cdk': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
+    '@angular-architects/module-federation': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/animations': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/cdk': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
     '@angular/common': {
       singleton: true,
       strictVersion: true,
-      requiredVersion: '^18.0.0',
+      requiredVersion: '^17.0.0 || ^18.0.0',
       includeSecondaries: {
         skip: ['@angular/common/http/testing']
       }
     },
-    '@angular/compiler': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/flex-layout': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/forms': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/material': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/material-moment-adapter': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/platform-browser': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
-    '@angular/platform-browser-dynamic': { singleton: true, strictVersion: true, requiredVersion: '^18.0.0' },
+    '@angular/compiler': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/flex-layout': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/forms': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/material': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/material-moment-adapter': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/platform-browser': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
+    '@angular/platform-browser-dynamic': { singleton: true, strictVersion: true, requiredVersion: '^17.0.0 || ^18.0.0' },
     rxjs: { singleton: true, strictVersion: true, requiredVersion: 'auto' }
   })
 });
