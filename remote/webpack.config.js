0f5d95bcf67f702d9022f2a95193e40631f96471
Papp Norbert <pnorbert@itron.com>
2024-07-11 23:34:45 +0200

    .

--- remote/webpack.config.js
+++ remote/webpack.config.js
@@ -1,15 +1,31 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const { withModuleFederationPlugin, share } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-
   name: 'remote',
-
   exposes: {
     './Component': '././src/app/app.component.ts',
   },
-
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
-
+  shared: share({
+    '@angular-architects/module-federation': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/animations': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/cdk': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+      includeSecondaries: {
+        skip: ['@angular/common/http/testing']
+      }
+    },
+    '@angular/compiler': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/flex-layout': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/forms': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/material': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/material-moment-adapter': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/platform-browser': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/platform-browser-dynamic': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    rxjs: { singleton: true, strictVersion: true, requiredVersion: 'auto' }
+  })
 });
